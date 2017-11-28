4c23f7ebe5020fcb9de06b7002000ea44519bf6d
Reges Eduardo Oberderfer Junior <regesober@gmail.com>
2017-11-28 15:12:40 -0200

    the COMPUTATION part of the simulation now uses Compose from library(functional) to call the entire chain of functions from a single function call

--- funcional/serio_sepa.R
+++ funcional/serio_sepa.R
@@ -1,5 +1,6 @@
 source('funcional/definitions.R')
 
+# Creates a hard-coded quadtree for tests
 root <- new_qnode(new_particle(new_point(10, 10), 20, new_point(0, 0), new_point(0, 0), 1024))
 root[[2]] <- new_particle(new_point(3, 3), 1, new_point(0, 0), new_point(0, 0), 0)
 root[[3]] <- new_particle(new_point(1, 1), 2, new_point(0, 0), new_point(0, 0), 0)
@@ -12,21 +13,25 @@ root[[5]][[4]][[3]] <- new_particle(new_point(1, 1), 6, new_point(0, 0), new_poi
 root[[5]][[5]] <- new_qnode(new_particle(new_point(40, 40), 40, new_point(0, 0), new_point(0, 0), 64))
 root[[5]][[5]][[2]] <- new_particle(new_point(3, 3), 8, new_point(0, 0), new_point(0, 0), 0)
 
+# Verifies the number of particles in the tree
 print("Number of particles: ")
 print(qnode_nof_particles(root))
 
-#print(computeMassDistribution(root))
 
-#root <- new_qnode(new_particle(new_point(10, 10), 20, new_point(0, 0), new_point(0, 0), 1024))
-#root[[2]] <- new_particle(new_point(1, 1), 2, new_point(0, 0), new_point(0, 0), 0)
-#root[[5]] <- new_qnode(new_particle(new_point(5, 5.5), 20, new_point(0, 0), new_point(0, 0), 256))
-#root[[5]][[2]] <- new_particle(new_point(1, 3), 4, new_point(0, 0), new_point(0, 0), 0)
-#root[[5]][[3]] <- new_particle(new_point(5, 5), 5, new_point(0, 0), new_point(0, 0), 0)
-md <- computeMassDistribution(root)
-cf <- computeForces(md)
-upv <- updatePositionAndVelocity(cf, 1)
-print(upv)
+################ READ THIS ################
+## The flow of this simulation is as follows:
+## GROUPING: The quadtree is built with at maximum one particle per quadrant
+## COMPUTATION: The new positions and velocities of the particles are calculated
+## PLOTTING: The particles' positions are plotted/updated
+################ READ THIS ################
 
-print("List of particles: ")
 
-print(qnode_toList(upv))
+## COMPUTATION :
+# defines the amount of time between each update
+simulationStep <- 1 # 1 second between each update
+# evaluates updatePositionAndVelocity for deltaT=simulationStep using Curry from library(functional)
+updatePositionAndVelocityForSimulationStep <- Curry(updatePositionAndVelocity, deltaT=simulationStep)
+# composes all functions of the COMPUTATION part into a single function, using Compose from library(functional)
+applySimulationStep <- Compose(computeMassDistribution, computeForces, updatePositionAndVelocityForSimulationStep, qnode_toList)
+
+applySimulationStep(root)
