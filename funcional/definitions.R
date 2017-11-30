62497d354314f567cafd01e9a9ea9a667cc5c536
Jose Pedro Martinez <jpsmartinez@inf.ufrgs.br>
2017-11-30 18:32:04 -0200

    o stress foi tanto que dei -am normalmente nao sou assim mas essa curva de aprendizado ta ingrime

--- funcional/definitions.R
+++ funcional/definitions.R
@@ -4,11 +4,85 @@ library(functional)
 # point (2D) : data.frame
 #   x : float, y : float
 new_point <- function(x, y) data.frame(x=x, y=y)
+point_bin_op <- function(p1, p2) new_point(op(p1["x"],p2["x"]), op(p1["x"], p2["x"]))
+point_quadrant_i <- function(point, quad_center) 
+  if (point["x"][[1]] <= quad_center["x"][[1]]) {
+    if(point["y"][[1]] <= quad_center["y"][[1]]) 1 else 2
+  } else {
+    if(point["y"][[1]] <= quad_center["y"][[1]]) 3 else 4
+  }
+point_subquadrant <- function(point, quad) quad_sub(quad, point_quadrant_i(point, quad_center(quad)))
+
+# quad : data.frame
+#   top_left : point
+#   bot_right : point
+new_quad <- function(top_left, size) data.frame(top_left=top_left, size=size)
+
+# scale a quad preserving its center
+quad_scale <- function(quad, s) {
+  shift <- (s-1)*quad["size"][[1]]
+  new_quad(new_point(quad["top_left.x"][[1]]+shift, quad["top_left.y"][[1]]+shift) , s*quad["size"])
+}
+quad_center <- function(quad) {
+  shift <- quad["size"][[1]]/2
+  new_point(quad["top_left.x"][[1]]+shift, quad["top_left.y"][[1]]+shift)
+  
+}
+quad_from_input <- function(particles) {
+  new_size <- function(top, bot) max(abs(top["x"] - bot["x"]), abs(top["y"] - bot["y"]))
+  
+  
+  best_point <- function(p,q,fun) new_point(fun(q["x"][[1]], p["x"][[1]]), fun(q["y"][[1]], p["y"][[1]]))
+  
+  points_from_particles <- function(particles) {
+    l <- length(particles)
+    
+    # a tem que dar [[1]] pra ele nao manter o namespace das chaves linguagem tao bonita mas tem uns over
+    if(l==0) list() else
+      c(list(new_point(particles[[1]]["point.x"][[1]], particles[[1]]["point.y"][[1]])), points_from_particles(tail(particles, l-1)))
+  }
+  
+  spread <- function (points, quad) {
+    l <- length(points)
+    tail <- tail(points, l-1)
+    
+    if (l == 0) quad else {
+      #h <- new_point(points[[1]]["point.x"], points[[1]]["point.y"])
+      h <- new_point(points[[1]]["x"][[1]], points[[1]]["y"][[1]])
+      print(quad)
+      quad_top <- new_point(quad["top_left.x"][[1]], quad["top_left.y"][[1]])
+      
+      toppest <- best_point(h, quad_top, min)
+      bottest <- best_point(h, new_point(quad["size"][[1]]+toppest["x"][[1]], quad["size"][[1]]+toppest["y"][[1]]), max)
+      
+      spread(tail, new_quad(
+        toppest,
+        new_size(toppest, bottest)
+      ))
+    }   
+  }
+  p <- points_from_particles(particles)
+  l <- length(p)
+  if (l == 0) new_quad(top_left=new_point(0,0), size=0) else
+    print(p)
+  spread(p, new_quad(top_left=new_point(p[[1]]["x"][[1]], p[[1]]["y"][[1]]), size=0))
+}
+quad_sub <- function(quad, sub_index) {
+  half_size <- quad["size"][[1]]/2
+  if (sub_index==1) new_quad(new_point(quad["top_left.x"][[1]], quad["top_left.y"][[1]]), half_size) else
+    if (sub_index==2) new_quad(new_point(quad["top_left.x"][[1]], half_size+quad["top_left.y"][[1]]), half_size) else
+      if (sub_index==3) new_quad(half_size+new_point(quad["top_left.x"][[1]], quad["top_left.y"][[1]]), half_size) else
+        if (sub_index==4) new_quad(half_size+new_point(quad["top_left.x"][[1]], half_size+quad["top_left.y"][[1]]), half_size)
+}
 
 # particle : data.frame
 #   point : point,
 #   PHYSICIS STUFF
 new_particle <- function(point, mass, velocity, force, quadrantSize, ...) data.frame(point=point, mass=mass, velocity=velocity, force=force, quadrantSize=quadrantSize)
+
+# ba mas que inferno na moral
+particle_set_qsize = function(particle, qsize) new_particle(particle_to_point(particle), particle["mass"][[1]], new_point(particle["velocity.x"][[1]], particle["velocity.y"][[1]]), new_point(particle["force.x"][[1]], particle["force.y"][[1]]), qsize)
+particle_to_point <- function(particle) new_point(particle["point.x"][[1]], particle["point.y"][[1]])
 
 # hm eh de boas aninhar data.frame, desde que a dimensionalidade do valor de cada coluna ("chave") 
 #   do data.frame tenha a mesma dimensionalidade
@@ -25,6 +99,41 @@ qnode_empty <- function(node) length(node) == 0
 qnode_childs <- function(node) if (!qnode_empty(node)) tail(node, length(node)-1)
 qnode_degree <- function(node) Reduce("+", lapply(qnode_childs(node), function(c) as.integer(!qnode_empty(c))))
 qnode_external <- function(node) qnode_degree(node) == 0
+
+# given a qnode (parent), a child index [[2..5]], and another qnode (child)
+#   returns a qnode similar to the parent, overwriting the child_index with child
+qnode_force_insert <- function (qnode, child_index, child) {
+  new_child <- function(qnode, index, child_index, child) if (index==child_index) child else qnode[[index]]
+  list(qnode[[1]], new_child(qnode, 2, child_index, child), new_child(qnode, 3, child_index, child), new_child(qnode, 4, child_index, child), new_child(qnode, 5, child_index, child))
+}
+
+# given a qnode, its quadrant info (top_left, size) and a particle
+#   returns a new qnode, with particle inserted at 
+qnode_insert <- function(qnode, rootquad, particle) {
+  if(qnode_empty(qnode)) new_qnode( # trivial
+    particle_set_qsize(particle, rootquad["size"][[1]])
+  ) else {
+    point <- particle_to_point(particle)
+    sub_quad <- point_subquadrant(point, rootquad)
+    particle_child_i <- 1+point_quadrant_i(point, quad_center(rootquad))
+    if(qnode_external(qnode)) 
+      qnode_insert(
+        qnode_force_insert(qnode, 1+point_quadrant_i(particle_to_point(qnode[[1]]), quad_center(rootquad)), # make current node internal
+                           new_qnode(particle_set_qsize(qnode[[1]], rootquad["size"]/2))), 
+        rootquad, particle # then try again
+      ) else qnode_force_insert(qnode, particle_child_i, qnode_insert(qnode[[particle_child_i]], sub_quad, particle)) # go deeper, hoping next node is empty
+    
+  }
+}
+
+list_toQnode <- function(particles) {
+  spread <- function(particles, rootquad) {
+    l <- length(particles) 
+    t <- tail(particles, l-1)
+    if (l==0) list() else qnode_insert(list_toQnode(t), rootquad, particles[[1]])
+  }
+  spread(particles, quad_from_input(particles))
+}
 
 # recursively counts the number of particles in a quadrant
 qnode_nof_particles <- function(node) sum(unlist(lapply(qnode_childs(node), function(c) {
@@ -163,56 +272,9 @@ distance <- function(node1, node2) ((qnode_x(node1) - qnode_x(node2))^2 + (qnode
 distance_x <- function(node1, node2) (qnode_x(node1) - qnode_x(node2))
 distance_y <- function(node1, node2) (qnode_y(node1) - qnode_y(node2))
 
-# quad : data.frame
-#   top_left : point
-#   bot_right : point
-new_quad <- function(top_left, size) data.frame(top_left=top_left, size=size)
 
-quad_from_input <- function(particles) {
-  new_size <- function(top, bot) {
-    max(abs(top["x"] - bot["x"]), abs(top["y"] - bot["y"]))
-    
-  }
-  
-  best_point <- function(p,q,fun) new_point(fun(q["top_left.x"], p["point.x"]), fun(q["top_left.y"], p["point.y"]))
-  
-  points_from_particles <- function(particles) {
-    l <- length(particles)
-    
-    if(l==0) list() else
-      c(list(new_point(particles[[1]]["point.x"], particles[[1]]["point.y"])), points_from_particles(tail(particles, l-1)))
-  }
-  
-  spread <- function (points, quad) {
-    l <- length(points)
-    tail <- tail(points, l-1)
-    #    print(quad)
-        print(points)
-    #    print(h)
-    #    print(h["point.x"][[1]])
-    
-    if (l == 0) quad else {
-      h <- new_point(points[[1]]["point.x"], points[[1]]["point.y"])
-      #print(h)
-      
-      old_bottest <- new_point(h["point.x"]+quad["size"], h["point.y"]+quad["size"])
-      toppest <- best_point(h, quad, min)
-      
-      spread(tail, new_quad(
-        toppest,
-        new_size(toppest, old_bottest)
-      ))
-    }   
-  }
-  spread(points_from_particles(particles), new_quad(top_left=new_point(0,0), size=0))
-}
 
-# given a qnode, its quadrant info (top_left, size) and a particle
-#   returns a new qnode, with particle inserted at 
-qnode_insert <- function(qnode, rootquad, particle) {
-  if(qnode_empty(qnode)) new_qnode(particle) else 
-    list()
-}
+
 
 updatePositionAndVelocity <- function(node, deltaT) {
   particle_setPositionAndVelocity <- function (particle, point, velocity) {
@@ -258,7 +320,8 @@ updatePositionAndVelocity <- function(node, deltaT) {
 # extracts a list of particles from a quadtree
 qnode_toList <- function (node) {
   if(qnode_empty(node)) list() else
-    c(list(node[[1]]), qnode_toList(node[[2]]), qnode_toList(node[[3]]), qnode_toList(node[[4]]), qnode_toList(node[[5]]))
+    if (qnode_external(node)) list(node[[1]]) else
+    c(qnode_toList(node[[2]]), qnode_toList(node[[3]]), qnode_toList(node[[4]]), qnode_toList(node[[5]]))
 }
 
 
