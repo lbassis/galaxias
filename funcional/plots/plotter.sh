aec8343fd530051f7fb7a31cca9e87dd2882016c
Jose Pedro Martinez <jpsmartinez@inf.ufrgs.br>
2017-12-01 22:22:27 -0200

    added titles to curves

--- funcional/plots/plotter.sh
+++ funcional/plots/plotter.sh
@@ -7,8 +7,8 @@ set title "N-body comparisons"
 set yrange[0:200]
 set xrange[0:20]
 
-plot "out.dat" using 1:2, \
-     x*(x-1)
+plot "out.dat" using 1:2 title "Samples", \
+     x*(x-1) title "Brute Force"
 
 #pause -1 "Hit any key to continue"
 
