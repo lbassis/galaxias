2f29b56fe2abe49818dde6bccb05feceed3f2ac8
Jose Pedro Martinez <jpsmartinez@inf.ufrgs.br>
2017-12-01 23:11:31 -0200

    nicer plot for experiment

--- funcional/plots/plotter.sh
+++ funcional/plots/plotter.sh
@@ -4,10 +4,10 @@ set output 'out.png'
 
 set title "N-body comparisons"
 
-set yrange[0:100]
-set xrange[0:10]
+set yrange[0:200]
+set xrange[0:25]
 
-plot "out.dat" using 1:2 title "Samples", \
+plot "out.dat" using 1:2 ps 5 title "Samples", \
      x*(x-1) title "Brute Force"
 
 #pause -1 "Hit any key to continue"
