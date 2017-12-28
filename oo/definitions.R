c1ebc997075e0b5c5dc93ed79d3c19150172cfb4
Lucas Barros de Assis <lbassis@inf.ufrgs.br>
2017-12-27 23:02:47 -0200

    learning how to work with methods

--- oo/definitions.R
+++ oo/definitions.R
@@ -1,5 +1,29 @@
+
+
+# definiçao da classe
 new_point <- setRefClass("new_point",
                           fields = list(x = "numeric", y = "numeric"))
+
+# definiçao dos metodos da classe
+new_point$methods(point_sum_op = function(p) {
+  new_point(x = .self$x[[1]] + p$x[[1]], y = .self$y[[1]] + p$y[[1]])
+})
+
+new_point$methods(point_sub_op = function(p) {
+  new_point(x = .self$x[[1]] - p$x[[1]], y = .self$y[[1]] - p$y[[1]])
+})
+
+new_point$methods(point_quadrant_i = function(quad_center) # supondo que quad_center é um ponto tb
+  if (.self$x[[1]] <= quad_center$x[[1]]) {
+    if(.self$y[[1]] <= quad_center$y[[1]]) 1 else 2
+  } else {
+    if(.self$y[[1]] <= quad_center$y[[1]]) 3 else 4
+  })
+
+
+
+
+
 
 new_quad <- setRefClass("new_quad",
                         fields = list(top_left = "new_point", size = "new_point"))
