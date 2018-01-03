ad9e2d673663b1102037b84adc58197ebea38e40
regesober <regesober@gmail.com>
2018-01-03 21:04:43 -0200

    added setters

--- oo/Particle.R
+++ oo/Particle.R
@@ -1,4 +1,5 @@
 source("Quad.R")
+library(data.table)
 
 Particle <- setRefClass("Particle",
   contains = "Point",
@@ -6,7 +7,7 @@ Particle <- setRefClass("Particle",
     mass = "numeric",
     velocity = "Point",
     force = "Point",
-    quadrant_size = "Point"
+    quadrant_size = "numeric"
   ),
   methods = list(
     get_mass = function() .self$mass,
@@ -17,15 +18,13 @@ Particle <- setRefClass("Particle",
     get_vy = function() .self$get_velocity()$get_y(),
     get_fx = function() .self$get_force()$get_x(),
     get_fy = function() .self$get_force()$get_y(),
-    particle_to_point = function() {
-      Point(x = .self$get_point()$get_x(), y = .self$get_point()$get_y())
+    to_point = function() {
+      Point(x = .self$get_x(), y = .self$get_y())
     },
-    particle_set_qsize = function(qsize) {
-      Particle(point = particle_to_point(), mass = .self$get_mass(), 
-                   velocity = Point(x = .self$get_vx, y = .self$get_vy()), 
-                   force = Point(x = .self$get_fx(), y = .self$get_fy()),
-                   quadrant_size = qsize[[1]])
-    }
+    set_mass = function(mass) .self$mass = mass,
+    set_velocity = function(velocity) .self$velocity = velocity,
+    set_force = function(force) .self$force = force,
+    set_quadrant_size = function(quadrant_size) .self$quadrant_size = quadrant_size
   )
       
 )
--- oo/Point.R
+++ oo/Point.R
@@ -7,22 +7,24 @@ Point <- setRefClass("Point",
   methods = list(
     get_x = function() return(.self$x),
     get_y = function() return(.self$y),
-    point_sum_op = function(p) {
+    sum_op = function(p) {
       Point(x = .self$x + p$x, y = .self$y + p$y)
     },
-    point_sub_op = function(p) {
+    sub_op = function(p) {
       Point(x = .self$x - p$x, y = .self$y - p$y)
     },
-    point_quadrant_i = function(quad_center) {# supondo que quad_center é um ponto tb
+    quadrant_i = function(quad_center) {# supondo que quad_center é um ponto tb
       if (.self$x <= quad_center$x) {
         if(.self$y <= quad_center$y) 1 else 2
       } else {
         if(.self$y <= quad_center$y) 3 else 4
       }
     },
-    point_subquadrant = function(quad) {
-      quad$quad_sub(.self$point_quadrant_i(quad$quad_center()))
-    }
+    subquadrant = function(quad) {
+      quad$quad_sub(.self$quadrant_i(quad$quad_center()))
+    },
+    set_x = function(x) .self$x = x,
+    set_y = function(y) .self$y = y
   )
 )
 
